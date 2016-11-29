98cd66663e871180f0d5e7c31a8e1c0e9aa9d96e
sawanoboly <sawanoboriyu@higanworks.com>
2016-11-29 16:33:43 +0900

    implement http://so-wh.at/entry/2016/04/30/Kumogata2

---
+++ lib/kumogata2/plugin/yaml.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+class Kumogata2::Plugin::YAML
+  Kumogata2::Plugin.register(:yaml, ['yaml', 'yml'], self)
+
+  def initialize(options)
+    @options = options
+  end
+
+  def parse(str)
+    YAML.load(str)
+  end
+
+  def dump(hash)
+    YAML.dump(hash).colorize_as(:yaml)
+  end
+end
